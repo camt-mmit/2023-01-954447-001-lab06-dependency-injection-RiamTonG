2d539b87f1b7624f3b0f586ff9c987aa9f8b4804
Taratip Cheawsinrapatham <taratiptongtong@gmail.com>
2023-08-07 18:29:31 +0700

    week06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-ng3';
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'example', pathMatch: 'full' },
+  {
+    path: 'example',
+    loadChildren: () => import('./example/routes').then((m) => m.routes),
+  },
+];
